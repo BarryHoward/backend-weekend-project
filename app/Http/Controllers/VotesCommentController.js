116bcb187a16f85902f13569dd8074ffb497f189
Barry Howard <BarryLeeHoward@gmail.com>
2016-11-12 13:58:54 -0500

    got voting to store in votes tables

--- app/Http/Controllers/VotesCommentController.js
+++ app/Http/Controllers/VotesCommentController.js
@@ -9,12 +9,23 @@ class VotesCommentController {
 		let linkId = request.param('link_id')
 		let commentId = request.param('comment_id')
 		let data = {}
-		data.user_id = user.id
-		data.links_id = linkId
+		data.user_id = Number(user.id)
+		data.links_id = Number(linkId)
 		data.comments_id = commentId
-		let vote = yield VotesComment.create(data)
-		vote.save()
-		response.status(201).json(vote)
+
+		const exists = yield VotesComment.query().table('votes_comments')
+			.where("user_id", data.user_id).andWhere("comments_id", data.comments_id)
+
+		if (exists.length === 0){
+			let vote = yield VotesComment.create(data)
+			vote.save()
+			response.status(201).json(vote)
+		} else {
+			response.status(403).json({text: "Can't vote for that again"})
+		}
+
+
+
 	}
 }
 
--- app/Http/Controllers/VotesLinkController.js
+++ app/Http/Controllers/VotesLinkController.js
@@ -8,11 +8,21 @@ class VotesLinkController {
 		let user = request.authUser
 		let linkId = request.param('link_id')
 		let data = {}
-		data.user_id = user.id
-		data.links_id = linkId
-		let vote = yield VotesLink.create(data)
-		vote.save()
-		response.status(201).json(vote)
+		data.user_id = Number(user.id)
+		data.links_id = Number(linkId)
+
+		const exists = yield VotesLink.query().table('votes_links')
+			.where("user_id", data.user_id).andWhere("links_id", data.links_id)
+
+		if (exists.length === 0){
+			let vote = yield VotesLink.create(data)
+			vote.save()
+			response.status(201).json(vote)
+		} else {
+			response.status(403).json({text: "Can't vote for that again"})
+		}
+
+
 
 	}
 
